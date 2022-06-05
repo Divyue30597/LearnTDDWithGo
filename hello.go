1afe1754133e5db18de616b64a5afe50ccd1440f
Divyue30597 <tomchi.sharma305@outlook.com>
2022-06-05 18:51:34 +0530

    first goLang commit

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func Hello() string {
+	return "Hello, world"
+}
+
+func Hello2(name string) string {
+	return "Hello, " + name
+}
+
+func main() {
+	fmt.Println(Hello())
+	fmt.Println(Hello2("Chris"))
+}
