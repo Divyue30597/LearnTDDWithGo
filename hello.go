93eb3c161b367129aac5f32cb90e95ba62eddf78
Divyue30597 <divyue.sharma@viacomcontractor.com>
2022-06-06 18:10:55 +0530

    hello world added in french and espanol

--- hello.go
+++ hello.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello() string {
 	return englishHelloPrefix + "world"
@@ -12,7 +14,33 @@ func Hello2(name string) string {
 	return englishHelloPrefix + name
 }
 
+func Hello3(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
+}
+
+func Hello4(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+
+	if language == "French" {
+		return frenchHelloPrefix + name
+	}
+
+	return englishHelloPrefix + name
+}
+
 func main() {
 	fmt.Println(Hello())
 	fmt.Println(Hello2("Chris"))
+	fmt.Println(Hello3(""))
+	fmt.Println(Hello4("Elodie", "Spanish"))
+	fmt.Println(Hello4("Divyue", "French"))
 }
--- hello_test.go
+++ hello_test.go
@@ -20,7 +20,8 @@ func TestHello2(t *testing.T) {
 	}
 }
 
-// Example of subtests 
+// Example of subtests. Here we are introducing subtests.
+// Sometimes it is useful to group tests around a thing and then have subtests describing different scenarios
 func TestHello3(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
 		got := Hello2("Chris")
@@ -32,11 +33,55 @@ func TestHello3(t *testing.T) {
 	})
 
 	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello2("")
+		got := Hello3("")
 		want := "Hello, World"
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	})
+}
+
+// The above code can further be refactored to the code below
+
+func TestHello4(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello2("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello3("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func TestHello5(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello4("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello4("Divyue", "French")
+		want := "Bonjour, Divyue"
+		assertCorrectMessage(t, got, want)
 	})
 }
