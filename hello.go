d2e41c62ae6089ade82e812d8aceb0f9fb7d70b0
Divyue30597 <divyue.sharma@viacomcontractor.com>
2022-06-06 21:32:02 +0530

    added switch

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
@@ -37,10 +41,59 @@ func Hello4(name string, language string) string {
 	return englishHelloPrefix + name
 }
 
+func Hello5(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	// replacing multiple if's with switch
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	}
+
+	return prefix + name
+}
+
+// above code further be refactored to two different functions
+
+func Hello6(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+// named return value as `prefix string`
+// This will create a variable called prefix in the function and will be assigned to "" for string in case of int it will be initialised to 0
+// we can return whatever it's set to by just calling `return` in the function rather than `return prefix`
+// 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
 func main() {
 	fmt.Println(Hello())
 	fmt.Println(Hello2("Chris"))
 	fmt.Println(Hello3(""))
 	fmt.Println(Hello4("Elodie", "Spanish"))
 	fmt.Println(Hello4("Divyue", "French"))
+	fmt.Println(Hello5("Tomchi", spanish))
+	fmt.Println(Hello5("Tom", french))
+	fmt.Println(Hello5("Tomchi", ""))
+	fmt.Println(Hello6("Tomchi", spanish))
+	fmt.Println(Hello6("Tom", french))
+	fmt.Println(Hello6("Tomchi", ""))
 }
