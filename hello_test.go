5ad8b37acf24e555af82c26248a49e4b83423e99
Divyue30597 <tomchi.sharma305@outlook.com>
2022-06-06 15:46:30 +0530

    added new tests

--- hello_test.go
+++ hello_test.go
@@ -20,3 +20,23 @@ func TestHello2(t *testing.T) {
 	}
 }
 
+// Example of subtests 
+func TestHello3(t *testing.T) {
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello2("Chris")
+		want := "Hello, Chris"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello2("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
